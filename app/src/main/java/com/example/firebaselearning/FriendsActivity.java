cb45fd2bf9acf305a8e3b15819c201a56c464c1a
1Divy1 <davidandreimesaros@gmail.com>
2022-07-23 22:48:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firebaselearning/FriendsActivity.java
@@ -0,0 +1,21 @@
+package com.example.firebaselearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class FriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/firebaselearning/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.firebaselearning;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText userNameText, emailText, passwordText;
+    private Button submitBtn;
+    private TextView registerLoginText;
+
+    private boolean bLoginScreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userNameText = findViewById(R.id.usernameText);
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.passwordText);
+        registerLoginText = findViewById(R.id.registerLoginText);
+        submitBtn = findViewById(R.id.submitBtn);
+
+        bLoginScreen = true;
+        
+        View view = null;
+        userNameText.setVisibility(view.GONE);
+        
+        registerLoginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(bLoginScreen == true) {
+                    // login screen
+                    bLoginScreen = false;
+                    submitBtn.setText("login");
+                    userNameText.setVisibility(v.GONE);
+                    registerLoginText.setText("Don't have an account? Register");
+                }
+                else if (bLoginScreen == false) {
+                    // register screen
+                    bLoginScreen = true;
+                    submitBtn.setText("register");
+                    userNameText.setVisibility(v.VISIBLE);
+                    registerLoginText.setText("Already have an account? Login");
+                }
+            }
+        });
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // check if user data is not empty
+                if(emailText.getText().toString().isEmpty() || passwordText.getText().toString().isEmpty()) {
+                    // check if we are in register mode
+                    if(bLoginScreen == false && userNameText.getText().toString().isEmpty()) {
+                        Toast.makeText(MainActivity.this, "Empty boxes not accepted", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                }
+
+                if(submitBtn.getText().toString().equals("login")) {
+                    // user logging in
+                    userLogin();
+                }
+                else if(submitBtn.getText().toString().equals("register")) {
+                    // user creating an account
+                    userRegister();
+                }
+            }
+        });
+
+    }
+
+    private void userLogin() {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(emailText.getText().toString().trim(), passwordText.getText().toString().trim())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            // open friends activity
+                            startActivity(new Intent(MainActivity.this, FriendsActivity.class));
+                            Toast.makeText(MainActivity.this, "Logged in successfully", Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void userRegister() {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailText.getText().toString(), passwordText.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            // open friends activity
+                            startActivity(new Intent(MainActivity.this, FriendsActivity.class));
+                            Toast.makeText(MainActivity.this, "Account created successfully", Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+}
